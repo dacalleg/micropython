a37f7883850d006c888be31287600b12d63ade9d
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2021-12-17 15:30:52 +0100

    Fix

--- ports/esp32/boards/NAVEL/mpconfigboard.cmake
+++ ports/esp32/boards/NAVEL/mpconfigboard.cmake
@@ -2,7 +2,7 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
     boards/sdkconfig.spiram
-    boards/sdkconfig/240mhz
+    boards/sdkconfig.240mhz
     boards/GENERIC_OTA/sdkconfig.board
 )
 
