37cdf6421bd4b882734a4f116958c9b89aa5d78a
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2022-03-10 14:50:43 +0100

    Added Encryption Board

--- build-navel.sh
+++ build-navel.sh
@@ -9,3 +9,5 @@ git submodule update
 git clone https://github.com/micropython/micropython-lib.git
 
 cd /micropython/ports/esp32 && make BORAD=NAVEL
+
+#echo "esptool.py -p /dev/ttyUSB0 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 ./build/bin/bootloader/bootloader.bin 0x8000 ./build/bin/partition_table/partition-table.bin 0xd000 ./build/bin/ota_data_initial.bin 0x10000 ./build/bin/micropython.bin"
